67d537acfb73f621f2bec408ed1153176ad2f207
zmafnan <iamzafn22@gmail.com>
2026-09-03 07:26:47 +0700

    fix

--- extract-files.py
+++ extract-files.py
@@ -39,6 +39,19 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    "vendor/bin/hw/android.hardware.security.keymint@1.0-service.mitee": blob_fixup()
+    .replace_needed(
+        "android.hardware.security.keymint-V1-ndk_platform.so",
+        "android.hardware.security.keymint-V3-ndk.so",
+    )
+    .replace_needed(
+        "android.hardware.security.sharedsecret-V1-ndk_platform.so",
+        "android.hardware.security.sharedsecret-V1-ndk.so",
+    )
+    .replace_needed(
+        "android.hardware.security.secureclock-V1-ndk_platform.so",
+        "android.hardware.security.secureclock-V1-ndk.so",
+    ),
     (
         "vendor/etc/init/hw/init.batterysecret.rc",
         "vendor/etc/init/hw/init.mi_thermald.rc",
