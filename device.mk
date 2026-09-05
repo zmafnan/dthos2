2f2787a09372ab8e2850c3ddf8bbe681958fb330
zmafnan <iamzafn22@gmail.com>
2026-09-06 03:49:12 +0700

    aristotle: Configure Lineage Health charging control and mgame thermal profile

--- device.mk
+++ device.mk
@@ -54,7 +54,13 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
-    android.hardware.health-service.mediatek-recovery
+    android.hardware.health-service.mediatek-recovery \
+    vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 # Power
 PRODUCT_PACKAGES += \
